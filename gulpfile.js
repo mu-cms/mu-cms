3b03470f1c66dae3106c0b422b6601305eed3aac
Mikael Karon <mikael@karon.se>
2018-09-06 17:33:18 +0200

    add comments

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,17 @@ const plugin = require('@mu-cms/gulp-ssr-angular');
 const renderFactory = require('angular-io-example/dist/render');
 
 gulp.task('default', function() {
+  // path to Angular index file
   const index = require.resolve('angular-io-example/dist/browser/index.html');
-
+  // Angular files relative to index
   const app = gulp.src(dirname(index) + '/**')
     .pipe(gulp.dest('dist'));
-
+  // static HTML files from routes
   const routes = gulp.src('routes/**/*.md')
     .pipe(plugin(renderFactory(readFileSync(index))))
     .pipe(rename({
       extname: '.html'
     }));
-
+  // merge and write to `dist`
   merge(app, routes).pipe(gulp.dest('dist'));
 });
